6b64957b528ec8e6dbdd117a30c9a9ec2eae7ae4
Jinwoo Hwang <hnan1118@ajou.ac.kr>
2019-07-09 17:25:17 +0900

    Initial commit

---
+++ src/main/java/kr/ac/ajou/LolCrawler/LolCrawlerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ajou.LolCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolCrawlerApplication.class, args);
+	}
+
+}
