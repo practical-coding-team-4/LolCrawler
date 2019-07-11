59736f06edf72af4dd479e91304ace20ce306658
Jinwoo Hwang <32378542+hnan1118@users.noreply.github.com>
2019-07-12 08:58:31 +0900

    Use domain objects for API call response (#3)
    * Declare LeaguePosition Class in domain package and replace String type with LeaguePosition
    
    * Add LeagueEntryDTO class in domain package and replace string return type with Set<LeagueEntryDTO>
    
    * Add MiniSeriesDTO in LeagueEntryDTO

--- src/main/java/kr/ac/ajou/LolCrawler/api/LolApiClient.java
+++ src/main/java/kr/ac/ajou/LolCrawler/api/LolApiClient.java
@@ -1,14 +1,18 @@
 package kr.ac.ajou.LolCrawler.api;
 
+import kr.ac.ajou.LolCrawler.domain.LeagueEntryDTO;
 import kr.ac.ajou.LolCrawler.domain.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
+import java.util.Set;
+
 @Service
 public class LolApiClient {
-    private final String api_key = "RGAPI-090ffbc6-a810-4fab-b6e8-e7e7f0561b3e";
+    private final String api_key = "RGAPI-156acf29-0820-441f-9435-42842b7b7584";
     private final String summonersApiUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}";
     private final String positionsApiUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={api_key}";
 
@@ -21,8 +25,10 @@ public class LolApiClient {
         return res.getId();
     }
 
-    public String requestLeaugePosition(String encryptedSummonerId) {
-        return restTemplate.exchange(positionsApiUrl, HttpMethod.GET, null, String.class, encryptedSummonerId, api_key)
-                .getBody();
+
+    public Set<LeagueEntryDTO> requestLeaugePosition(String encryptedSummonerId) {
+        Set<LeagueEntryDTO> leaguePosition = restTemplate.exchange(positionsApiUrl, HttpMethod.GET, null, Set.class, encryptedSummonerId, api_key)
+        .getBody();
+        return leaguePosition;
     }
 }
---
+++ src/main/java/kr/ac/ajou/LolCrawler/domain/LeagueEntryDTO.java
@@ -0,0 +1,31 @@
+package kr.ac.ajou.LolCrawler.domain;
+
+import lombok.Data;
+
+
+@Data
+public class LeagueEntryDTO{
+    private String queueType;
+    private String summonerName;
+    private boolean hotStreak;
+    class MiniSeriesDTO {
+        String progress;
+        int losses;
+        int target;
+        int wins;
+    }
+    private MiniSeriesDTO miniSeries;
+    private int wins;
+    private boolean veteran;
+    private int losses;
+    private String rank;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String tier;
+    private String summonerId;
+    private int leaguePoints;
+
+
+}
+
