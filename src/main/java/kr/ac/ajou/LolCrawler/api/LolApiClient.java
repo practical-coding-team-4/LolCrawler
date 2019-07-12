5896c6db965a51505a39fb6f6e3748da25f0b783
kimminseok <rjwltps57@ajou.ac.kr>
2019-07-12 16:57:39 +0900

    add MongoConfig Class and Mongo Dependency

--- src/main/java/kr/ac/ajou/LolCrawler/api/LolApiClient.java
+++ src/main/java/kr/ac/ajou/LolCrawler/api/LolApiClient.java
@@ -12,7 +12,7 @@ import java.util.Set;
 
 @Service
 public class LolApiClient {
-    private final String api_key = "RGAPI-156acf29-0820-441f-9435-42842b7b7584";
+    private final String api_key = "RGAPI-b073a3dc-8185-497a-be87-2e6f2437f7ef";
     private final String summonersApiUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}";
     private final String positionsApiUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={api_key}";
 
