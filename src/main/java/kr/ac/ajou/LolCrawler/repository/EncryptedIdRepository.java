379714d689f14df9a37a49ef3d10cf0ebe1fea1b
JuHyeongLee <dlwngud24@naver.com>
2019-07-15 15:52:34 +0900

    Add updateLeagueEntryDTO function in Repository (not finish)

--- src/main/java/kr/ac/ajou/LolCrawler/repository/EncryptedIdRepository.java
+++ src/main/java/kr/ac/ajou/LolCrawler/repository/EncryptedIdRepository.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,7 +20,31 @@ public class EncryptedIdRepository {
     }
 
     public void updateLeagueEntryDTO(List<LeagueEntryDTO> leagueEntryDTOS){
+        for(int i=0 ; i<leagueEntryDTOS.size();i++) {
+            LeagueEntryDTO leagueEntry = leagueEntryDTOS.get(i);
 
+            String summonerId = leagueEntry.getSummonerId();
+            String queueType = leagueEntry.getQueueType();
+            Query query = new Query();
+            query.addCriteria(Criteria.where("summonerId").is(summonerId));
+            query.addCriteria(Criteria.where("queueType").is(queueType));
+
+            Update update = new Update();
+            update.set("summonerName", leagueEntry.getSummonerName());
+            update.set("hotStreak", leagueEntry.isHotStreak());
+            update.set("wins", leagueEntry.getWins());
+            update.set("veteran", leagueEntry.isVeteran());
+            update.set("losses", leagueEntry.getLosses());
+            update.set("rank", leagueEntry.getRank());
+            update.set("leagueId", leagueEntry.getLeagueId());
+            update.set("inactive", leagueEntry.isInactive());
+            update.set("freshBlood", leagueEntry.isFreshBlood());
+            update.set("tier", leagueEntry.getTier());
+            update.set("summonerId", leagueEntry.getSummonerId());
+            update.set("leaguePoints", leagueEntry.getLeaguePoints());
+            mongoTemplate.updateFirst(query, update, LeagueEntryDTO.class);
+
+        }
     }
 
     public LeagueEntryDTO findLeagueEntryDTO(String encryptedSummonerId){
